1e132d3c6d6af6e793f59fac8dc0ad17457d41ec
Lukas Crockett <lukas.crockett@gmail.com>
2025-10-08 20:36:40 -0600

    prob 2 3

--- project1/mlp_prob2.py
+++ project1/mlp_prob2.py
@@ -6,16 +6,12 @@ class MLP(eqx.Module):
 
     def __init__(self, key):
         key1, key2, key3 = jax.random.split(key,3)
-        self.layers = (eqx.nn.Linear(2, 200, key=key1),
+        self.layers = (eqx.nn.Linear(3, 200, key=key1),
                        jax.nn.tanh,
                        eqx.nn.Linear(200, 100, key=key2),
                        jax.nn.relu,
                        eqx.nn.Linear(100, 2, key=key3))
         
-#    def __call__(self, x):
-#        for layer in self.layers[:-1]:
-#            x = layer(x)
-#        return self.layers[-1](x)
     def __call__(self, x):
         for layer in self.layers:
             x = layer(x)
